f8a7abe7af2624cd3d609409fe5cf25aa96ddc8f
yamam00s <s.yamamo0327@gmail.com>
2019-05-12 16:07:28 +0900

    一元管理するべきstateをstore/indexに追加

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,7 +6,18 @@ import mutations from './mutations';
 
 Vue.use(Vuex);
 
+// 状態`Auth`と状態`Board`をVuexのstateで一元管理できるようにする
+const state = {
+  auth: { // 状態`Atuh`
+    token: null, // `tokenはnullで初期化`
+    userId: null // `userIdはnullで初期化`
+  },
+  board: { // 状態`Board`
+    lists: [] // 状態`TaskListは空で初期化`
+  }
+}
 export default new Vuex.Store({
+  state, // 定義したstateを`state`オプションに指定
   getters,
   actions,
   mutations,
