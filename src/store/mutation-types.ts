ac362a11002aa563b236588978af0bf63b91f8f0
yamam00s <s.yamamo0327@gmail.com>
2019-05-15 23:42:23 +0900

    データフロー周りの雛形コードのセットアップ

---
+++ src/store/mutation-types.ts
@@ -0,0 +1,6 @@
+export const AUTH_LOGIN: String = 'AUTH_LOGIN';
+export const FETCH_ALL_TASKLIST: String = 'FETCH_ALL_TASKLIST';
+export const ADD_TASK: String = 'ADD_TASK';
+export const UPDATE_TASK: String = 'UPDATE_TASK';
+export const REMOVE_TASK:String = 'REMOVE_TASK';
+export const AUTH_LOGOUT:String = 'AUTH_LOGOUT'
