1de7d8983a4916f6f8996ea9741273b0c8d87d40
yamam00s <s.yamamo0327@gmail.com>
2019-06-16 13:36:15 +0900

    vue-routerを型アサーション

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,7 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import Router, { RouterOptions } from 'vue-router';
 import routes from './router';
-
 Vue.use(Router);
 
 // TSのエラー解消が必要
-export default new Router({ routes });
+export default new Router({routes} as RouterOptions);
