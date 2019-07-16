5a1b3aa4902b5737c52002c54ff0572c9c573125
yamam00s <s.yamamo0327@gmail.com>
2019-07-17 01:20:05 +0900

    add KbnButton

---
+++ src/components/atoms/KbnButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <button
+    :class="classes"
+    :disabled="disabled"
+    type="button"
+    @click="handleClick"
+  >
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({})
+
+export default class KbnButton extends Vue {
+  @Prop({ default: 'button' })
+  type!: string
+
+  @Prop({ default: false })
+  disabled!: boolean
+
+  handleClick(ev: unknown) {
+    this.$emit('click', ev)
+  }
+
+  get classes(): string[] {
+    const cls = this.type === 'text' ? `-${this.type}` : ''
+    return [`kbn-button${cls}`]
+  }
+};
+</script>
+
+<style scoped>
+.kbn-button {
+  padding: .6em 1.3em;
+}
+.kbn-button-text {
+  border: none;
+  padding-left: 0;
+  padding-right: 0;
+}
+</style>
