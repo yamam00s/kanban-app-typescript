5cee9c126fda6ed4b465686e11aa95b4fd9e5ca0
yamam00s <s.yamamo0327@gmail.com>
2019-07-30 00:55:14 +0900

    create KbnLoginView

--- src/components/templates/KbnLoginView.vue
+++ src/components/templates/KbnLoginView.vue
@@ -1,3 +1,36 @@
 <template>
-  <p>ログインページ</p>
+  <div class="login-view">
+    <h1>kanban App</h1>
+    <KbnLoginForm :onlogin="handleLogin"></KbnLoginForm>
+  </div>
 </template>
+
+<script>
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import KbnLoginForm from '@/components/molecules/KbnLoginForm.vue';
+
+@Component({
+  components: {
+    KbnLoginForm
+  }
+})
+export default class KbnLoginView extends Vue {
+  handleLogin(authInfo) {
+    return this.$store.dispatch('login', authInfo)
+      .then(() => {
+        this.$router.push({ path: '/' })
+      })
+      .catch(err => this.throwReject(err))
+  }
+  throwReject(err) {
+    return Promise.reject(err)
+  }
+}
+</script>
+
+<style scoped>
+.login-view {
+  width: 320px;
+  margin: auto;
+}
+</style>
